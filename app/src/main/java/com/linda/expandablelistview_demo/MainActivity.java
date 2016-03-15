b5ef09747b55afd09d618683b3d97adcec7688b6
linda <linda.zhou@taosales.cc>
2016-03-15 17:20:49 +0800

    默认展开第一组，并且点开某一组的同时关闭其他组

--- app/src/main/java/com/linda/expandablelistview_demo/MainActivity.java
+++ app/src/main/java/com/linda/expandablelistview_demo/MainActivity.java
@@ -19,6 +19,7 @@ import java.util.Map;
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +27,9 @@ public class MainActivity extends Activity {
         setUpView();
     }
 
-    private void setUpView(){
+    private void setUpView() {
 
-        ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
+        final ExpandableListView expandableListView = (ExpandableListView) findViewById(R.id.expandableListView);
 
         final List<String> parentList = new ArrayList<>();
         parentList.add("parent1");
@@ -38,7 +39,7 @@ public class MainActivity extends Activity {
         parentList.add("parent5");
         parentList.add("parent6");
 
-        final Map<String,List<String>> map = new HashMap<>();
+        final Map<String, List<String>> map = new HashMap<>();
 
         List<String> childList1 = new ArrayList<>();
         childList1.add("parent1-child1");
@@ -47,7 +48,7 @@ public class MainActivity extends Activity {
         childList1.add("parent1-child4");
         childList1.add("parent1-child5");
         childList1.add("parent1-child6");
-        map.put("parent1",childList1);
+        map.put("parent1", childList1);
 
         List<String> childList2 = new ArrayList<>();
         childList2.add("parent2-child1");
@@ -56,7 +57,7 @@ public class MainActivity extends Activity {
         childList2.add("parent2-child4");
         childList2.add("parent2-child5");
         childList2.add("parent2-child6");
-        map.put("parent2",childList2);
+        map.put("parent2", childList2);
 
         List<String> childList3 = new ArrayList<>();
         childList3.add("parent3-child1");
@@ -65,7 +66,7 @@ public class MainActivity extends Activity {
         childList3.add("parent3-child4");
         childList3.add("parent3-child5");
         childList3.add("parent3-child6");
-        map.put("parent3",childList3);
+        map.put("parent3", childList3);
 
         List<String> childList4 = new ArrayList<>();
         childList4.add("parent4-child1");
@@ -74,7 +75,7 @@ public class MainActivity extends Activity {
         childList4.add("parent4-child4");
         childList4.add("parent4-child5");
         childList4.add("parent4-child6");
-        map.put("parent4",childList4);
+        map.put("parent4", childList4);
 
         List<String> childList5 = new ArrayList<>();
         childList5.add("parent5-child1");
@@ -83,7 +84,7 @@ public class MainActivity extends Activity {
         childList5.add("parent5-child4");
         childList5.add("parent5-child5");
         childList5.add("parent5-child6");
-        map.put("parent5",childList5);
+        map.put("parent5", childList5);
 
         List<String> childList6 = new ArrayList<>();
         childList6.add("parent6-child1");
@@ -92,14 +93,29 @@ public class MainActivity extends Activity {
         childList6.add("parent6-child4");
         childList6.add("parent6-child5");
         childList6.add("parent6-child6");
-        map.put("parent6",childList6);
+        map.put("parent6", childList6);
 
-        expandableListView.setAdapter(new MyAdapter(parentList,map));
+        expandableListView.setAdapter(new MyAdapter(parentList, map));
+
+        expandableListView.expandGroup(0);//默认展开第一组
+
+        //点开一组的时候，其他组关闭
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                for (int i = 0, count = expandableListView.getExpandableListAdapter().getGroupCount(); i < count; i++) {
+
+                    if (groupPosition != i){
+                        expandableListView.collapseGroup(i);
+                    }
+                }
+            }
+        });
 
         expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
             public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
-                Toast.makeText(MainActivity.this,"click + "+ (map.get(parentList.get(groupPosition)).get(childPosition)),Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "click + " + (map.get(parentList.get(groupPosition)).get(childPosition)), Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
@@ -107,7 +123,7 @@ public class MainActivity extends Activity {
         expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
             @Override
             public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                Toast.makeText(MainActivity.this,"click + "+ parentList.get(groupPosition),Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "click + " + parentList.get(groupPosition), Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
